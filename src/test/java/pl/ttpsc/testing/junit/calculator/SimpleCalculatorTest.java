48c74bc94d5af1ece6df9fb289efb18831f88a5d
Kamil <203901@edu.p.lodz.pl>
2018-03-12 09:51:44 +0100

    Some tests on lab

--- src/test/java/pl/ttpsc/testing/junit/calculator/SimpleCalculatorTest.java
+++ src/test/java/pl/ttpsc/testing/junit/calculator/SimpleCalculatorTest.java
@@ -1,4 +1,145 @@
 package pl.ttpsc.testing.junit.calculator;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.assertj.core.api.Assertions;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnitParamsRunner.class)
 public class SimpleCalculatorTest {
+
+    public static long startTime;
+    SimpleCalculator sut;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @BeforeClass
+    public static void beforeClass() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Total time: " + (System.currentTimeMillis() - startTime) + " ms.");
+    }
+
+    static Object[] primeNumbers() {
+        return $(
+                $(12, false),
+                $(7, true),
+                $(15, false),
+                $(3, true)
+        );
+    }
+
+
+    @Before
+    public void setUp() {
+        sut = new SimpleCalculator();
+    }
+
+    @Test
+    public void shouldAddTwoNumbers() {
+        //given
+        double a = 1.0;
+        double b = 9.0;
+
+        //when
+        double actual = sut.add(a, b);
+
+        //then
+        assertEquals(10.0, actual, 0.0000001);
+    }
+
+    @Test
+    public void shouldSubtractTwoNumbers() {
+        //given
+        double a = 3.0;
+        double b = 1.0;
+
+        //when
+        double actual = sut.subtract(a, b);
+
+        //then
+        assertEquals("Some assertion error", 2.0, actual, 0.000001);
+    }
+
+    @Test
+    //@Ignore
+    @Parameters({"1", "3", "7", "11"})
+    public void shouldDetermineWhenNumberIsPrime(int number) {
+        //given
+
+        //when
+        boolean prime = sut.isPrime(number);
+
+        //then
+        assertTrue(prime);
+    }
+
+    @Test
+    //@Ignore
+    @Parameters(method = "primeNumbers")
+    public void shouldDetermineWhenNumberIsPrime(int number, boolean primeFrag) {
+        //given
+
+        //when
+        boolean prime = sut.isPrime(number);
+
+        //then
+        assertEquals(primeFrag, prime);
+    }
+
+    @Test(expected = CannotDivideByZeroException.class)
+    public void shouldNotDivideByZero() throws CannotDivideByZeroException {
+        //given
+        double a = 2.0;
+        double b = 0.0;
+
+        //when
+        sut.divide(a, b);
+    }
+
+    @Test
+    public void shouldNotDivideByZeroWithExpected() throws CannotDivideByZeroException {
+        //given
+        double a = 2.0;
+        double b = 0.0;
+
+        //expect
+        expectedException.expect(CannotDivideByZeroException.class);
+
+        //when
+        sut.divide(a, b);
+    }
+
+    @Test
+    public void shouldNotDivideByZeroWithAssert() {
+        //given
+        double a = 2.0;
+        double b = 0.0;
+
+        //expect
+        assertThatExceptionOfType(CannotDivideByZeroException.class)
+                .isThrownBy(() -> sut.divide(a,b));
+
+        //when
+        assertThatThrownBy(() -> sut.divide(a,b))
+                .isInstanceOf(CannotDivideByZeroException.class);
+
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+    }
 }
--- src/test/java/pl/ttpsc/testing/junit/stack/StackExerciseTest.java
+++ src/test/java/pl/ttpsc/testing/junit/stack/StackExerciseTest.java
@@ -1,5 +1,137 @@
 package pl.ttpsc.testing.junit.stack;
 
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnitParamsRunner.class)
 public class StackExerciseTest {
+    public static long startTime;
+    StackExercise stackut;
+
+    @BeforeClass
+    public static void beforeClass() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Total time: " + (System.currentTimeMillis() - startTime) + " ms.");
+    }
+
+    static Object[] primeNumbers() {
+        return $(
+                $(12, false),
+                $(7, true),
+                $(15, false),
+                $(3, true)
+        );
+    }
+
+    @After
+    public void tearDown() {
+        stackut = null;
+    }
+
+
+    @Before
+    public void setUp() {
+        stackut = new StackExercise();
+    }
+
+    @Test
+    public void shouldGetStringOnTop() {
+        //given
+        String a = "someString";
+
+        //when
+        stackut.push(a);
+
+        //then
+        try {
+            assertEquals(a, stackut.top());
+        } catch (StackEmptyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldGetStringsInCorrectOrder() {
+        //given
+        String a = "firstString";
+        String b = "secondString";
+
+        //when
+        stackut.push(a);
+        stackut.push(b);
+        String first = null;
+        String second = null;
+
+        try {
+            first = stackut.pop();
+            second = stackut.pop();
+        } catch (StackEmptyException e) {
+            e.printStackTrace();
+        }
+
+        //then
+        assertEquals(a, second);
+        assertEquals(b, first);
+    }
+
+    @Test
+    //@Ignore
+    @Parameters({"some", "diffrent", "strings", "11"})
+    public void shouldGetDifferentStrings(String string) {
+        //given
+        String a = "firstString";
+
+        //when
+        stackut.push(a);
+        String first = null;
+
+        try {
+            first = stackut.pop();
+        } catch (StackEmptyException e) {
+            e.printStackTrace();
+        }
+
+        //then
+        assertEquals(string, first);
+    }
+
+    @Test
+    //@Ignore
+    @Parameters(method = "stringFrag")
+    public void shouldGetDifferentStrings(String string, boolean stringFrag) {
+        //given
+        String a = "firstString";
+
+        //when
+        stackut.push(a);
+
+        String first = null;
+        try {
+            first = stackut.pop();
+        } catch (StackEmptyException e) {
+            e.printStackTrace();
+        }
+
+        //then
+        assertEquals(stringFrag, string);
+    }
+
+    Object[] stringFrag() {
+        return $(
+                $("I", false),
+                $("like", false),
+                $("summer")
+        );
+    }
 }
